ee8cee38247b219a66266bdd9bc368cca77191d9
Seuketchi <tristan.jadman@g.msuiit.edu.ph>
2025-01-20 11:51:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# avr-asm-practice" 
+"# avr-asm-practice" 
---
+++ assembly/main.asm
@@ -0,0 +1,22 @@
+;
+; assembly.asm
+;
+; Created: 17/01/2025 7:08:51 pm
+; Author : jadma
+;
+
+
+; Replace with your application code
+main:
+    ldi r17, 5
+	ldi r18, 5
+	sub r17, r18
+	brne end
+	inc r18
+
+
+
+
+end:
+	nop
+	
